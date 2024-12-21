1acec07e51de05100305c49301bb7025d71d3823
jamalnasser12 <jamalnasser320@gmail.com>
2024-12-21 19:18:51 +0200

    added a score system📜

--- script/script.js
+++ script/script.js
@@ -2,6 +2,11 @@ const choices=["rock","paper","scissor"];
 const playerDisplay =document.getElementById("playerdisplay");
 const computerDisplay=document.getElementById("computerdisplay");
 const result=document.getElementById("result");
+const playerScoreDisplay = document.getElementById("playerScore");
+const computerScoreDisplay = document.getElementById("computerScore");
+
+let playerScore = 0;  
+let computerScore = 0; 
 
 function playgame(playerchoice){
     const computerchoice=choices[Math.floor(Math.random()*3)];
@@ -15,14 +20,19 @@ function playgame(playerchoice){
 
             case"rock":
             resultTest=(computerchoice==="scissors")?"winner winner chicken dinner 🍗":"you lose try again dont lose hope🤕";
+            if (computerchoice === "scissors") playerScore++; else computerScore++;
             break;
 
             case"paper":
             resultTest=(computerchoice==="rock")?"winner winner chicken dinner 🍗":"you lose try again dont lose hope🤕";
+            if (computerchoice === "rock") playerScore++; else computerScore++;
+
             break;
 
             case"scissor":
             resultTest=(computerchoice==="paper")?"winner winner chicken dinner 🍗":"you lose try again dont lose hope🤕";
+            if (computerchoice === "paper") playerScore++; else computerScore++;
+
             break;
 
 
@@ -31,4 +41,8 @@ function playgame(playerchoice){
     playerDisplay.textContent=`player: ${playerchoice}`;
     computerDisplay.textContent=`computer: ${computerchoice}`;
     result.textContent=resultTest;
+
+
+    playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
 }
